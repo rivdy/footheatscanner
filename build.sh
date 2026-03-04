a6adc855520dcb548009011c048fb7f1ee316276
RPR <rivaldy.rivly@gmail.com>
2026-03-04 23:11:21 +0700

    tambahan di build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Gunakan DATABASE_PUBLIC_URL saat build (internal URL belum tersedia di build phase)
+if [ -n "$DATABASE_PUBLIC_URL" ]; then
+  DATABASE_URL=$DATABASE_PUBLIC_URL python manage.py migrate
+else
+  python manage.py migrate
+fi
